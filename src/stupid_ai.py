efc9b79090411cf45b6c91d174c86ac731c67acc
August Rouvari <august.rouvari@helsinki.fi>
2025-03-20 19:13:57 +0200

    Add Reference Project

---
+++ src/stupid_ai.py
@@ -0,0 +1,44 @@
+import random
+import chess
+import time
+
+def set_board(board: chess.Board, board_position:str):
+    print(f"Set board to {board_position}!")
+    board.set_fen(board_position)
+
+def make_move(board: chess.Board):
+    legal_moves = [move.uci() for move in list(board.legal_moves)]
+    print(f"I found {len(legal_moves)} legal moves: {', '.join(legal_moves)}")
+    choice = random.choice(legal_moves)
+    board.push_uci(choice)
+
+    return choice
+
+def main():
+
+    board = chess.Board()
+
+    while True:
+        opponent_move = input()
+        time.sleep(random.randrange(1,10)/100)
+        if opponent_move.startswith("BOARD:"):
+            set_board(board, opponent_move.removeprefix("BOARD:"))
+        elif opponent_move.startswith("RESET:"):
+            board.reset()
+            print("Board reset!")
+        elif opponent_move.startswith("PLAY:"):
+            choice = make_move(board)
+            # example about logs
+            print(f"I chose {choice}!")
+            # example about posting a move
+            print(f"MOVE:{choice}")
+        elif opponent_move.startswith("MOVE:"):
+            move = opponent_move.removeprefix("MOVE:")
+            board.push_uci(move)
+            print(f"Received move: {move}")
+        else:
+            print(f"Unknown tag: {opponent_move}")
+            break
+
+if __name__ == "__main__":
+    main()
