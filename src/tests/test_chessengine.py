dc5cc5c8166770a86e5dadcb65e271f7ab47c0e1
August Rouvari <august.rouvari@helsinki.fi>
2025-04-05 21:41:23 +0300

    Add Tests For Checks

--- src/tests/test_chessengine.py
+++ src/tests/test_chessengine.py
@@ -56,3 +56,18 @@ def test_valid_moves_(engine):
     moves = engine.validMoves()
     assert len(moves) == 20
     assert all(isinstance(move, Move) for move in moves)
+
+#Checking that there are no checks at the start of the game.
+def test_in_check_start(engine):
+    assert not engine.isInCheck()
+
+#Putting pieces in a position where black queen is checking the white king and using isInCheck function
+#for validating that there is a check.
+def test_check_detection(engine):
+    engine.board[7][4] = " "
+    engine.board[4][4] = "K"
+    engine.board[0][4] = " "
+    engine.board[3][4] = "k"
+    engine.board[1][4] = " "
+    engine.board[4][5] = "q"
+    assert engine.isInCheck()
